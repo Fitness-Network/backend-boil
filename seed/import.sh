55ef5087b12db1f41942c6b01830c131a2ad64d8
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-09-01 11:23:47 +0700

    [Add] Generate script

--- seed/import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-uri=mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@mongodb:27017/$MONGO_INITDB_DATABASE?authSource=admin
-
-echo $uri
-
-mongorestore --uri $uri $1 /mongo-seed/$APP_MODE/dump/$MONGO_INITDB_DATABASE
